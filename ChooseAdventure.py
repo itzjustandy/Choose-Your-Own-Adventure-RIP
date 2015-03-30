46703fc4b7a7da3508575cee6d9f6a27e50db0d4
SGIgnacio <ignas928@outlook.com>
2015-03-30 14:36:50 -0700

    Update ChooseAdventure.py

--- ChooseAdventure.py
+++ ChooseAdventure.py
@@ -14,26 +14,26 @@ w.pack()
 
 def intro():
     """ Introductory Function -> starts the story going """
-    messagebox.showinfo("Awake", "You find yourself slowly being stirred awake.\n" +
+    choice = simpledialog.askinteger("Awake", "You find yourself slowly being stirred awake.\n" +
                         "You open your eyes to find yourself lying on the ground of an unknown\n" + 
                         "room. Sitting up, you take a look around the room. From the ceiling\n" +
                         "down to the floor it is made entirely out of concrete and is empty,\n" +
                         "except for two sets of stairs and a window. Try and try as you might you\n" +
                         "you just can't seem to remember how exactly you ended up where you are.\n" +
                         "You push those thoughts aside and stand up, making your way to only\n" +
-                        "window in the room. Looking out 
-    
-    choice = simpledialog.askinteger("Choose wisely",
-                                   "You have a choice to pick: 1 or 2.")
-    if choice == 1:
-        choice1()
+                        "window in the room. Looking out the window, your eyes widen as you see\n" +
+                        "nothing but skyscrapers in the surrounding area. More confused than\n" +
+                        "ever, you step away from the window and decide what you should do next.\n\n" +
+                        "Should you:\n 1: Go upstairs. \n 2: Go downstairs.")
+    if (choice == 1):
+        upstairs()
     elif choice == 2:
-        choice2()
+        downstairs()
     else:
         intro()
 
 ################ Student A Functions #####################
-def choice1():
+def upstairs():
     choice = simpledialog.askinteger("Choose wisely",
                                      "This is the next part of the story.  Now you must choose 1 or 2 again.")
     if (choice == 1):
